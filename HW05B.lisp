67d9b2f0e907b89ae52f53f261da906fea055893
kyletruong <kyletruong@gmail.com>
2020-04-21 18:36:07 -0700

    finish actions and result; refactor terminal-test and utility

--- HW05B.lisp
+++ HW05B.lisp
@@ -1,34 +1,59 @@
 ; Developed by:
 ; Taha Hasan Syed
-;
-;
+; Kyle Truong
+; 
 
-(defun terminal-test(statlist)
-   (if (member 'b statlist)
-      (return-from terminal-test nil)
-   (return-from terminal-test t)
-   )
+
+; Set values for actions based on index
+(defvar A1 0)
+(defvar A2 1)
+(defvar A3 2)
+(defvar actions '(A1 A2 A3))
+
+
+; TERMINAL-TEST
+(defun terminal-test(statelist)
+  (cond
+    ((equalp 'b (car (member 'b statelist)))
+     nil)
+    (t t)
+  )
 )
 
-(defun utility(statlist)
-   (if (= (loop for i in statlist count (equalp 'a i)) 3)
-      (return-from utility 1)
-   )
-   
-   (if (= (loop for i in statlist count (equalp 'c i)) 3)
-      (return-from utility -1)
-   )
 
-   (return-from utility 0)
+; UTILITY
+(defun utility(statelist)
+  (cond
+    ((= (loop for state in statelist count (equalp 'a state)) 3)
+     1)
+    ((= (loop for state in statelist count (equalp 'c state)) 3)
+     -1)
+    (t 0)
+  )
 )
 
-(defun actions(statlist)
 
+; ACTIONS
+(defun actions(statelist)
+  (loop for state in statelist
+        for action in actions
+        if (equalp 'b state)
+        collect action
+  )
 )
 
-(defun result()
 
+; RESULT
+(defun result(statelist action)
+  (loop for state in statelist
+        for i from 0 to 2
+        if (equalp i action)
+        collect 'A
+        if (not (equalp i action))
+        collect state
+  )
 )
+
 
 ;test cases to check if functions work
 ;test cases will be removed later as it is not a requirement in the assignment
@@ -50,3 +75,26 @@
 (terpri)
 (write-line "List containing any combinations:")
 (write (utility (list 'a 'b 'c)))
+(terpri)
+(terpri)
+
+; Testing ACTIONS
+(write-line "Testing ACTIONS function")
+(format t "(B C B) => ~S" (actions '(B C B)))
+(terpri)
+(format t "(A C B) => ~S" (actions '(A C B)))
+(terpri)
+(format t "(B B B) => ~S" (actions '(B B B)))
+(terpri)
+(format t "(A C C) => ~S" (actions '(A C C)))
+(terpri)
+(terpri)
+
+; Testing RESULT
+(write-line "Testing RESULT function")
+(format t "(B C B), A3 => ~S" (result '(B C B) A3))
+(terpri)
+(format t "(A A A), A1 => ~S" (result '(A A A) A1))
+(terpri)
+(format t "(C B A), A2 => ~S" (result '(C B A) A2))
+(terpri)
